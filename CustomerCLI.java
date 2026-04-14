4f35fd3f16ab6e8cb22a7171b5c77bb2c44658d7
Arthur <arthuradams8@icloud.com>
2026-04-14 14:37:07 +0100

    small details

--- CustomerCLI.java
+++ CustomerCLI.java
@@ -34,7 +34,7 @@ public class CustomerCLI {
         		
         	case 2: 
         		System.out.printf("\n");
-        		System.out.println("ADD TO BASKET\n");
+        		System.out.println("ADD TO CART\n");
         		System.out.println("Please enter the name of the product to add:");
         		String productName = consoleInput.nextLine().trim();
         		customerInst.addToCart(productName);
@@ -43,7 +43,7 @@ public class CustomerCLI {
         		
         	case 3:
         		System.out.printf("\n");
-        		System.out.println("SHOPPING BASKET\n");
+        		System.out.println("SHOPPING CART\n");
         		customerInst.viewCart();
         		System.out.printf("\n");
         		break;
@@ -113,7 +113,7 @@ public class CustomerCLI {
         	case 5:
         		
         		System.out.printf("\n");
-        		System.out.println("CANCEL BASKET");
+        		System.out.println("CANCEL CART");
         		customerInst.cancelCart(consoleInput);
         		break;
         		
@@ -148,10 +148,10 @@ public class CustomerCLI {
     private static void printCustomerMenu() {
         System.out.println("PLEASE SELECT ACTION BY INPUTTING THE CORRESPONDING NUMBER (or 0 for logout)");
         System.out.println("1) View all products");
-        System.out.println("2) Add product to shopping basket");
-        System.out.println("3) View contents of shopping basket");
-        System.out.println("4) Purchase items in the basket");
-        System.out.println("5) Cancel shopping basket");
+        System.out.println("2) Add product to shopping cart");
+        System.out.println("3) View contents of shopping cart");
+        System.out.println("4) Purchase items in the cart");
+        System.out.println("5) Cancel shopping cart");
         System.out.println("6) Lookup with product ID");
         System.out.println("7) Search/filter based on compatilbility");
         System.out.println("0) Log out");
